45de58ab6653df9681857c3b1984d339a0ed8c18
Joel Filho <joelfilho02812@gmail.com>
2023-10-11 17:12:12 -0300

    created project

---
+++ src/main/java/br/com/joelbrs/jpawebservice/JpaWebserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelbrs.jpawebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWebserviceApplication.class, args);
+	}
+
+}
