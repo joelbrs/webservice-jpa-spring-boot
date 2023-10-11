ca701308db02b9c21597572216bd8e23d3215b60
Joel Filho <joelfilho02812@gmail.com>
2023-10-11 18:11:17 -0300

    fix: service findById

--- src/main/java/br/com/joelbrs/jpawebservice/services/UserService.java
+++ src/main/java/br/com/joelbrs/jpawebservice/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,8 +22,8 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public User findById(Long id) {
-        User user = repository.findById(id).orElse(null);
+        Optional<User> user = repository.findById(id);
 
-        return user;
+        return user.get();
     }
 }
